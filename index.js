e743a7769e68f1da49c2fdfe29303676ed8c94dd
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2021-08-17 08:22:29 -0600

    Added more dependicies that we needed

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const pug = require('pug');
 const routes = require('./routes/routes');
 const path = require('path');
-
+const bcrypt = require('bcryptjs');
 const app = express();
+
+let salt = bcrypt.genSaltSync(10);
+let hash = bcrypt.hashSync('bacon', salt);
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
