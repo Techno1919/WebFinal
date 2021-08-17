d391ab685307f90f0f3816493ac61ef989472479
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2021-08-17 08:17:55 -0600

    Have the index.js set up and the basics of the routes.js setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pug = require('pug');
+const routes = require('./routes/routes');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+
+
+app.listen(3000);
---
+++ routes/routes.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://MStewart:Mancer16@cluster0.acfi9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'Connection Error'));
+mdb.once('open', callback => {});
+
+let loginSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    email: String,
+    age: Number,
+    answer1 : String,
+    answer2 : String,
+    answer3 : String
+});
+
+let Login = mongoose.model('Login_Collection', loginSchema);
