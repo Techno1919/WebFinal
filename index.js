1c1399e7d2547961f3385246f59ed67fe37e8662
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2021-08-23 08:49:08 -0600

    Everything Done

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const pug = require('pug');
 const routes = require('./routes/routes');
 const path = require('path');
 const expressSession = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(expressSession({
     secret: 'whatever',
@@ -31,8 +34,8 @@ const checkAuth = (req, res, next) => {
 
 
 app.get('/', routes.index);
-app.get('/userpage', routes.userpage);
-app.post('/userpage', urlencodedParser, routes.update);
+app.get('/userpage/:id', routes.userpage);
+app.post('/userpage/:id', urlencodedParser, routes.update);
 app.post('/login', urlencodedParser, routes.login);
 app.get('/create', routes.createAccount);
 app.post('/create', urlencodedParser, routes.create);
--- routes/routes.js
+++ routes/routes.js
@@ -15,6 +15,9 @@ let mdb = mongoose.connection;
 mdb.on('error', console.error.bind(console, 'Connection Error'));
 mdb.once('open', callback => {});
 
+let loggedIn = false;
+let vistied = "";
+
 let personInfoSchema = mongoose.Schema({
     username: String,
     password: String,
@@ -29,51 +32,49 @@ let PersonInfo = mongoose.model('Login_Collection', personInfoSchema);
 let salt = bcrypt.genSaltSync(10);
 
 exports.userpage = (req, res) => {
-    PersonInfo.findById(req.params.id, (err, person) => {
-        if(err) return console.error(err);
-        res.render('edit', {
-            title: 'Update Account',
-            person
+    if(loggedIn)
+    {
+        PersonInfo.findById(req.params.id, (err, person) => {
+            if(err) return console.error(err);
+            res.render('userpage', {
+                person
+            });
         });
-    });
+    }
+    else
+    {
+        redirect('/');
+    }
+    
 }
 
-exports.editPerson = (req, res) => {
-    Person.findById(req.params.id, (err, person) => {
-        if(err) return console.error(err);
-        person.name = req.body.name;
-        person.age = req.body.age;
-        person.species = req.body.species;
-
-        person.save((err, person) => {
-            if(err) return console.error(err);
-            console.log(req.body.name + 'updated!');
-        })
-        res.redirect('/');
-    });
-};
-
 exports.update = (req, res) => {
+    console.log(req.body.password);
     let updatedPassword = bcrypt.hashSync(req.body.password, salt);
     PersonInfo.findById(req.params.id, (err, person) => {
         if(err) return console.error(err);
-        username = req.body.username;
-        password = updatedPassword;
-        email = req.body.email;
-        age = req.body.age;
-        answer1 = req.body.question1;
-        answer2 = req.body.question2;
-        answer3 = req.body.question3;
+        person.username = req.body.username;
+        person.password = updatedPassword;
+        person.email = req.body.email;
+        person.age = req.body.age;
+        person.answer1 = req.body.question1;
+        person.answer2 = req.body.question2;
+        person.answer3 = req.body.question3;
 
         person.save((err, person) => {
             if(err) return console.error(err);
             console.log('User updated!');
         })
-        res.redirect('/userpage');
+        res.render('userpage', {
+            person
+        });
     });
 };
 
 exports.index = (req, res) => {
+    let today = new Date();
+    vistied = today.getDate();
+    res.cookie('visited', vistied, {maxAge: 9999999999999999999999});
     res.render('index');
 };
 
@@ -87,12 +88,14 @@ exports.login = (req, res) => {
             console.log(compared);
             if(compared)
             {
+                loggedIn = true;
                 req.session.user = {
                     isAuthenticated: true,
                     username: req.body.username
                 };
                 res.render('userpage', {
-                    person: login[0]
+                    person: login[0],
+                    visited: req.cookies.visited
                 });
             }
             else
